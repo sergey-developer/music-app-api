95fa8407a34406392ac3c20b563a336bcbc80cf0
sergey-developer <ijrtre123@gmail.com>
2021-09-01 21:39:12 +0600

    refactoring user;

--- src/app/api/user/repository/interface.ts
+++ src/app/api/user/repository/interface.ts
@@ -1,9 +1,13 @@
-import { CreateUserDto } from 'api/user/dto'
 import { IUserDocument } from 'api/user/model'
+import { ICreateUserServicePayload } from 'api/user/service'
 import { MaybeNull } from 'shared/interface/utils/common'
 
+export interface ICreateUserRepositoryPayload
+  extends ICreateUserServicePayload {}
+
 export interface IUserRepository {
-  create: (payload: CreateUserDto) => Promise<IUserDocument>
+  create: (payload: ICreateUserRepositoryPayload) => Promise<IUserDocument>
+
   findOneByEmail: (
     email: IUserDocument['email'],
   ) => Promise<MaybeNull<IUserDocument>>
--- src/app/api/user/service/interface.ts
+++ src/app/api/user/service/interface.ts
@@ -1,6 +1,11 @@
+import { CreateUserDto } from 'api/user/dto'
+import { IUserDocument } from 'api/user/model'
 import { IUserRepository } from 'api/user/repository'
 
+export interface ICreateUserServicePayload extends CreateUserDto {}
+
 export interface IUserService {
-  create: IUserRepository['create']
+  create: (payload: ICreateUserServicePayload) => Promise<IUserDocument>
+
   getOneByEmail: IUserRepository['findOneByEmail']
 }
